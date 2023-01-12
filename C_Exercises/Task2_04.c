c36e3a17698dad41d67e554bda3caa09609c39b9
Nenad Jakovchevski <119482796+Nan0NJ@users.noreply.github.com>
2023-01-12 23:05:57 +0100

    Added Countdown

---
+++ C_Exercises/Task2_04.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main() {
+ 	printf("Enter time\n");
+ 	int CurH = 0, CurMin = 0;
+ 	int DeH = 0, DeMin = 0;
+ 	printf("Current Hour: ");
+ 	scanf("%d", &CurH);
+ 	printf("Current Minute: ");
+ 	scanf("%d", &CurMin);
+ 	printf("Deadline Hour: ");
+ 	scanf("%d", &DeH);
+ 	printf("Deadline Minute: ");
+ 	scanf("%d", &DeMin);
+ 	if(CurH<0 || CurH>24 || DeH<0 || DeH>24 || CurMin<0 || CurMin>60 || DeMin<0 || DeMin>60){
+ 		printf("Sorry Buddy, Time doesn't work like that!\n");
+ 		return 1;
+ 	}
+ 	int FullCurrentTime = (CurH*60)+CurMin;
+ 	int DeadLineTime = (DeH*60)+DeMin;
+ 	int Countdown = abs(FullCurrentTime - DeadLineTime);
+ 	printf("You have %d minutes left.\n", Countdown);
+  	return 0;
+ }
