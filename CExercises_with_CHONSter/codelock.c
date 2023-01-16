06559c48ebccf0c82c548de0b51cd9cfd26f0062
David <rezzyze@gmail.com>
2023-01-17 00:35:42 +0100

    Add files via upload

---
+++ CExercises_with_CHONSter/codelock.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main () {
+
+	char Majmune[10];
+	int array[10];
+
+	printf("\nEnter your password Majmune: ");
+	scanf("%s", Majmune);
+
+	int n = strlen(Majmune);
+
+	for ( int i=0; i<n; i++ ) {
+		int pepe = (int)(Majmune[i])-48;
+		array[i] = (pepe+5)%10;
+	}
+
+	printf("\nThe password is ");
+	for ( int i=0; i<n; i++ ) {
+		printf("%d", array[i]);
+	}
+
+	return 0;
+}
